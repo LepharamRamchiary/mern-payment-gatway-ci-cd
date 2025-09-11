6628883abda1a6eb225c2466cc0409333d0128d4
LepharamRamchiary <lepharamchiary@gmail.com>
2025-09-11 22:29:52 +0530

    added api for logout user

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
-import { registerUser, login } from "../controllers/user.controllers.js";
+import { registerUser, login, logout } from "../controllers/user.controllers.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -8,5 +9,8 @@ router
   .route("/register")
   .post(upload.fields([{ name: "avatar", maxCount: 1 }]), registerUser);
 router.route("/login").post(login);
+
+// Protected routes
+router.route("/logout").post(verifyJWT, logout);
 
 export default router;
