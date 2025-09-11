2584d9a44c457664190cbf7e12b2d57a5e9ccaa1
LepharamRamchiary <lepharamchiary@gmail.com>
2025-09-11 12:10:01 +0530

    .

---
+++ server/src/index.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+  path: ".env",
+});
+
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running at port: ${process.env.PORT}`);
+      console.log(`Swagger docs available at http://localhost:${process.env.PORT}/api-docs`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed !!", err);
+  });
---
+++ server/src/utils/swagger.js
@@ -0,0 +1,28 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from "swagger-ui-express";
+
+
+const swaggerDefinition = {
+  openapi: '3.0.0', 
+  info: {
+    title: 'E-commerce API', 
+    version: '1.0.0', 
+    description: 'E-commerce API documentation', 
+  },
+  servers: [
+    {
+      url: 'http://localhost:8000', 
+    },
+  ],
+};
+
+const options = {
+  swaggerDefinition,
+  apis: ['./src/routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+export default (app) => {
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
