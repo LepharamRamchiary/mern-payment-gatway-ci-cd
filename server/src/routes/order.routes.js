ebdac6f7f3932c9699a74247d4984ef22f1ec5e9
LepharamRamchiary <lepharamchiary@gmail.com>
2025-09-15 11:36:37 +0530

    added payment verify for order

--- server/src/routes/order.routes.js
+++ server/src/routes/order.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   createOrder,
+  verifyPayment
 } from "../controllers/order.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -10,6 +11,7 @@ router.use(verifyJWT);
 
 
 router.route("/create").post(createOrder);
+router.route("/verify").post(verifyPayment);
 
 
 export default router;
